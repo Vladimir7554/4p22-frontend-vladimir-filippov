18b5e2f3c3c528644ee64f38162f06a052c7f86b
Vladimir <vladimir@zabweb.ru>
2022-11-03 22:44:58 +0700

    added marks for homework_9 after teacher's notes

--- homework_9/script.js
+++ homework_9/script.js
@@ -1,11 +1,12 @@
-const getFibonacci = ((a = 0, b = 1) => () => {
-
+const getFibonacci = ((a = 0, b = 1) => () => { 
   const c = a + b;
   const inputNode = document.getElementById('input');
+
   a = b;
   b = c;
   inputNode.value = a;
   console.log(a);
+
   return a;
 
 })()
